2b80b41923e5a638270691ed95779c143c027c68
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-01-28 10:50:31 +0530

    worked on oneCard reading page

--- frontend/src/Componenets/Navigation/Navigation.js
+++ frontend/src/Componenets/Navigation/Navigation.js
@@ -2,15 +2,14 @@ import React from 'react'
 import './Navigation.css';
 import back_arrow from '../../assets/back_arrow.png'
 import About_section from '../../assets/about_section.png'
-import shuffle from '../../assets/shuffle.png';
-import animation_change from '../../assets/animation_change.png';
-import FontAwesomeIcon from 'react-icons'
-const Navigation = ({link}) => {
+import shuffle from '../../assets/shuffle_card.png';
+import change_animation from '../../assets/change_animation.png'
+const Navigation = ({link,shuffleCards}) => {
   return (
     <div className='Navigation-container'>
       <div><a href={link}><img src={back_arrow} className='Navigation-image-style'></img></a></div>
-      <div className='icon-container'><a href='#' className='icon-link'><i className="fa-duotone fa-regular fa-shuffle"></i></a></div>
-      <div className='icon-container'><a href='#' className='icon-link'><i class="fa-regular fa-diagram-next "></i></a></div>
+      <div ><button className='shuffle-button' onClick={shuffleCards}><img src={shuffle} className='Navigation-image-style' alt="Shuffle" /></button></div>
+      <div ><button className='shuffle-button' ><img src={change_animation} className='Navigation-image-style' alt="Shuffle" /></button></div>
       <div><a href={link}><img src={About_section} className='Navigation-image-style'></img></a></div>
     </div>
   )
--- frontend/src/Componenets/oneCard/oneCard.js
+++ frontend/src/Componenets/oneCard/oneCard.js
@@ -1,62 +1,58 @@
-import React, { useRef, useState } from 'react';
-
+import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import './oneCard.css';
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import Navigation from '../Navigation/Navigation';
+import { cardData } from '../../constants/CardData';
+
 export default function App() {
+  const [shuffledIndices, setShuffledIndices] = useState(Array.from({ length: cardData.length }, (_, index) => index));
+
+  const shuffleCards = () => {
+    const shuffled = shuffledIndices.slice();
+    for (let i = shuffled.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    setShuffledIndices(shuffled);
+  };
+
   return (
     <>
-      <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://res.cloudinary.com/dwiil16oj/image/upload/v1737625336/00_Back_card_upxqkz.jpg" />
-        </SwiperSlide>
-      </Swiper>
+      <div className='oneCard-container'>
+        <Swiper
+          effect={'coverflow'}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={'auto'}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className="mySwiper"
+        >
+          {shuffledIndices.map((index) => (
+            <SwiperSlide key={index}>
+              <div className='card' onClick={() => console.log(`Card clicked: ${index}`)}>
+                <img className='front' src={cardData[index].frontImage} alt="Front Image" />
+                <img className='back' src={cardData[index].backImage} alt="Back Image" />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className='card-choosed-box'>
 
-      <Navigation link={'/begin-new-cards'}/>
+        </div>
+      </div>
+      <Navigation link={'/begin-new-cards'} shuffleCards={shuffleCards} />
     </>
   );
 }
